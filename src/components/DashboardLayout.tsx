076462eacf6ec6abdc484b3164f7893f73e8e3da
Paulinne-Cintra <paulinnecinntra@outlook.com>
2025-05-22 17:58:15 -0300

    DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SidebarDashboard from './SidebarDashboard';
+
+const DashboardLayout: React.FC = () => {
+  return (
+    <div className="flex min-h-screen bg-residuall-gray-light">
+      <SidebarDashboard />
+      <div className="flex-1 flex flex-col">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
 
