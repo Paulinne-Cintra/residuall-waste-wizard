aff5389c5e0e1a3322a248641d8798addc491f8a
Paulinne-Cintra <paulinnecinntra@outlook.com>
2025-05-23 19:15:20 -0300

    DashboardLayout.tsx

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,14 +1,18 @@
-
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SidebarDashboard from './SidebarDashboard';
 
 const DashboardLayout = () => {
   return (
-    <div className="flex min-h-screen bg-residuall-gray-light">
+    // REMOVIDA a classe bg-residuall-gray-light daqui.
+    // O fundo do body (com o gradiente animado) agora será visível.
+    <div className="flex min-h-screen"> 
       <SidebarDashboard />
       <main className="flex-1 flex flex-col">
-        <Outlet />
+        {/* Você pode querer adicionar um padding aqui se suas páginas não tiverem um próprio */}
+        {/* <div className="p-6">  <- Exemplo de como adicionar padding se necessário */}
+          <Outlet />
+        {/* </div> */}
       </main>
     </div>
   );
